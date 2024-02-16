92e63e4eb5a1c11afadc5613ab8985dd6996c7a9
keyur-v-crest <keyur.vaghasiya@crestinfosystems.com>
2024-02-16 16:39:08 +0530

    Update user model

--- GC/settings.py
+++ GC/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.user'
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# Update use authentication model 
+AUTH_USER_MODEL = 'user.Details'
 
 
 # Internationalization
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin 
+from django.utils import timezone
 
-# Create your models here.
+class Details(AbstractUser, PermissionsMixin): 
+    created_at = models.DateTimeField(auto_now_add = True )
+    updated_at = models.DateTimeField(auto_now = True) 
+    dob = models.CharField(max_length = 100, default = None) 
+    address = models.CharField(max_length = 500, default = None, null = True)
+    gender = models.CharField(max_length = 100)
+    family_id = models.CharField(max_length = 100, default = None)
+    profession = models.CharField(max_length = 100, default = None)
+    profession_description = models.CharField(max_length = 500, default = None)
+
+    def save(self, *args, **kwargs): 
+        self.updated_at = timezone.now() 
+        super().save(**args, **kwargs)
