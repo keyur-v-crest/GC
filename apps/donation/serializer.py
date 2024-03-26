c220fe28af1f0ff3f338624909c6029b84849373
keyur-v-crest <keyur.vaghasiya@crestinfosystems.com>
2024-03-26 21:59:16 +0530

    Insert donation list fetch related route

--- apps/donation/serializer.py
+++ apps/donation/serializer.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers 
+from apps.donation.models import Details as Donation_details
+
+class DonationListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Donation_details
+        fields = ["id", "image", "organizer_image", "organizer_name", "donation_name", "donation_target"]
+
--- apps/donation/views.py
+++ apps/donation/views.py
@@ -4,6 +4,8 @@ from apps.user.helpers import CheckUserAuthentication
 from rest_framework.response import Response
 from apps.donation.models import Details as Donation_details 
 from datetime import datetime
+from django.core.paginator import Paginator, EmptyPage
+from apps.donation import serializer
 
 @api_view(["GET"])
 @authentication_classes([JWTAuthentication])
@@ -15,10 +17,19 @@ def donation_list_view(request):
         page_size = int(request.query_params.get("page_size"))
 
         Donation_list = Donation_details.objects.filter(donation_end_date__gte=today_date, donation_start_date__lte=today_date).order_by("-id")
-        print(Donation_list)
+        Donation_list_paginator = Paginator(Donation_list, page_size)
+
+        try:
+            Donation_list_paginator_page = Donation_list_paginator.page(page_number)
+        except EmptyPage:
+            Donation_list_paginator_page = []
+
+        Donation_list_paginator_page_data = serializer.DonationListSerializer(Donation_list_paginator_page, many = True)
+
         return Response({
             "status": True,
-            "message": "Fetch"
+            "message": "Fetch", 
+            "data": Donation_list_paginator_page_data.data
         }, status=200)
     except Exception as e:
         return Response({
